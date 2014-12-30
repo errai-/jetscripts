1dcdea71236eb3d7286ccad188bf733aa6d7a460
Hannu Siikonen <hannu.siikonen@aalto.fi>
2014-12-30 21:18:39 +0200

    README corrections and added CondFormats to analysis

---
+++ data_analysis/analyzer/CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.d
@@ -0,0 +1,29 @@
+
+# DO NOT DELETE
+
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: CondFormats/JetMETObjects/interface/JetCorrectionUncertainty.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: CondFormats/JetMETObjects/interface/SimpleJetCorrectionUncertainty.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: CondFormats/JetMETObjects/interface/JetCorrectorParameters.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/PtEtaPhiE4D.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/PtEtaPhiE4D.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/Math.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/etaMax.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/GenVector_exception.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/PxPyPzE4D.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/eta.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/Vector3D.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/Vector3Dfwd.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/Cartesian3D.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/Polar3Dfwd.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/CylindricalEta3D.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/Polar3D.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/Cylindrical3D.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/DisplacementVector3D.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/PositionVector3Dfwd.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/GenVectorIO.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/BitReproducible.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/CoordinateSystemTags.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/LorentzVector.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/Math/GenVector/LorentzVector.h
+./CondFormats/JetMETObjects/src/JetCorrectionUncertainty_cc.so: /home/hannu/root/include/cintdictversion.h /home/hannu/root/include/RVersion.h
+JetCorrectionUncertainty_cc__ROOTBUILDVERSION= 5.34/19
---
+++ data_analysis/analyzer/CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.d
@@ -0,0 +1,31 @@
+
+# DO NOT DELETE
+
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: CondFormats/JetMETObjects/interface/SimpleJetCorrector.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TFormula.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TNamed.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TObject.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/Rtypes.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/RConfig.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/RVersion.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/DllImport.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/Rtypeinfo.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/snprintf.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/strlcpy.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TGenericClassInfo.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TSchemaHelper.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TStorage.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TVersionCheck.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/Riosfwd.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TBuffer.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TString.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TMathBase.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TBits.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TObjArray.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TSeqCollection.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TCollection.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/TIterator.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: CondFormats/JetMETObjects/interface/JetCorrectorParameters.h
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: CondFormats/JetMETObjects/src/Utilities.cc
+./CondFormats/JetMETObjects/src/SimpleJetCorrector_cc.so: /home/hannu/root/include/cintdictversion.h /home/hannu/root/include/RVersion.h
+SimpleJetCorrector_cc__ROOTBUILDVERSION= 5.34/19
