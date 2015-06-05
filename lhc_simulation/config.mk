5ca0e0ba5844e39e7fb69becf4fc70e6a5677b35
Hannu Siikonen <hannu.siikonen@aalto.fi>
2015-06-05 12:22:28 +0300

    Updated the project to use clang as a default

--- lhc_simulation/config.mk
+++ lhc_simulation/config.mk
@@ -4,20 +4,21 @@ SHELL = /bin/sh
 
 #######################################
 # Using g++, the usual case:
-CXX = g++
-CFLAGS :=
+# CXX := g++
+# CFLAGS :=
 # Using clang, more hardcore debugging:
-#CXX = clang
-#CFLAGS := -v -fsanitize=undefined 
+CXX := clang++
+CFLAGS := -fsanitize=undefined
 #######################################
 
 CFLAGS := $(CFLAGS) -g -Wall -Wextra -Werror -Wshadow -Wno-error=unused-parameter -Wno-error=unused-function -Wno-error=unknown-pragmas -march=native -O3 -pedantic -m64
 CXXFLAGS := $(CFLAGS) -std=c++11
 
-PYTHIA6=-lEG -lEGPythia6 -L/home/hannu/Cern/pythia6 -lPythia6  
-PYTHIA8=$(shell pythia8-config --cflags --libs)
-FASTJET=$(shell fastjet-config --cxxflags --libs)
-ROOT=$(shell root-config --cflags --glibs) -lGenVector
+PYTHIA6:=-lEG -lEGPythia6 -L/home/hannu/Cern/pythia6 -lPythia6
+PYTHIA8:=$(shell pythia8-config --cflags --libs)
+FASTJET:=$(shell fastjet-config --cxxflags --libs)
+ROOT:=$(shell root-config --cflags --glibs) -lGenVector
+ROOT_INCLUDE:=$(shell root-config --cflags)
 CINTERPRET=$(ROOTSYS)/bin/rootcling
 # ROOT 5:
 #CINTERPRET=$(shell $(ROOTSYS)/bin/rootcint)
