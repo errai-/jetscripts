9dab2ff547b967df4de4c0c788773d6c19519923
Hannu Siikonen <hannu.siikonen@aalto.fi>
2015-06-10 11:14:51 +0300

    True clean-up and enhancement of the Herwig structures

--- lhc_simulation/herwig/StoreParticles.cc
+++ lhc_simulation/herwig/StoreParticles.cc
@@ -1,10 +1,4 @@
 #include "StoreParticles.h"
-#include "ThePEG/Interface/ClassDocumentation.h"
-#include "ThePEG/EventRecord/Particle.h"
-#include "ThePEG/Repository/UseRandom.h"
-#include "ThePEG/Repository/EventGenerator.h"
-#include "ThePEG/Utilities/DescribeClass.h"
-#include "ThePEG/PDT/StandardMatchers.h"
 
 using namespace jetanalysis;
 using namespace ThePEG;
@@ -17,108 +11,133 @@ using namespace ThePEG;
 #endif
 
 #include <iostream>
-#include <fstream>
 #include <string>
-
-#include "../generic/help_functions.h"
-#include "herwig_functions.h"
+#include <cmath>
 
 using std::cout;
 using std::endl;
 using std::string;
 
-int StoreParticles::getStatusCode(tPPtr part) const
+/* A function that checks whether a photon is originated from a pi0 and that
+ * the energy of the photon-pair corresponds to the pion. returns 0 if
+ * the origin is not a pion with good energy and 1 if it is */
+int StoreParticles::gammaChecker(const tPPtr& photon) {
+    
+    /* One mother, which is pi0 */
+    const tParticleVector parents = photon->parents();
+    if ( parents.size()!=1 || parents[0]->id()!=ParticleID::pi0 ) return 0;
+    
+    const ParticleVector children = parents[0]->children();
+    if ( children.size()!=2 ) return 0;
+    
+    double eDifference = fabs( parents[0]->momentum().t() -
+        children[0]->momentum().t() - children[1]->momentum().t() );
+    if ( eDifference > 0.001 ) return 0;
+    
+    return 1;
+}
+
+
+/* See HadronAndPartonSelector.cc in cmssw, indicates whether a hadron (used for 
+ * flavour inspection) is in an excited state or not. Checks whether a hadron has 
+ * a daughter of the same flavour. The parameter quarkId is a PDG quark flavour. */
+int StoreParticles::isExcitedHadronState(const tPPtr& part, int quarkId) {
+    assert( quarkId>=0 && quarkId<=6 );
+
+    ParticleVector children = part->children();
+    for (ParticleVector::const_iterator child = children.begin();
+            child != children.end(); ++child ) 
+        {
+        if ( HadrFuncs::StatusCheck( quarkId, (*child)->id() ) ) return 1;
+    }
+    return 0;
+}
+
+void StoreParticles::particleAdd(const tPPtr& part, int saveStatus) 
 {
-   int status = 1;
-   size_t nChildren = part->children().size();
-   if ( nChildren > 0 || part->next() ) {
-      tStepPtr step = part->birthStep();
-      if ((!step || (step && (!step->handler() || step->handler() == eh))) && part->id() != 82)
-         status = 3;
-      else
-         status = 2;
-   }
-   /* This is the official ThePEG method for obtaining status, but it does not
-    * work with the CMSSW interpretation of jet flavours. */
-   // if ( nChildren > 0 || part->next() ) status = 11; 
-   // if ( nChildren > 1 ) { 
-   //    long id = part->data().id();
-   //    if ( BaryonMatcher::Check(id) || MesonMatcher::Check(id) ||
-   //       id == ParticleID::muminus || id == ParticleID::muplus ||
-   //       id == ParticleID::tauminus || id == ParticleID::tauplus )
-   //          if ( part->mass() <= part->data().massMax() &&
-   //             part->mass() >= part->data().massMin() ) status = 2;
-   // }
-   return status;
+    pEvent->AddPrtcl(part->momentum().x(),part->momentum().y(),part->momentum().z(),
+                     part->momentum().t(),part->id(),part->data().charge(), saveStatus);
+}
+
+/* Implemented similarly as in cmssw. Not in active use, kept for reference. */
+int StoreParticles::getStatusCode(const tPPtr& part) const
+{
+    int status = 1;
+    if ( !part->children().empty() || part->next() ) {
+        tStepPtr step = part->birthStep();
+        if ((!step || (step && (!step->handler() || step->handler() == eh))) 
+            && part->id() != 82)
+        {
+            status = 3;
+        } else {
+            status = 2;
+        }
+    }
+    return status;
 }
   
 void StoreParticles::analyze(tEventPtr event, long ieve, int loop, int status) 
 {
-   /* Rotate to CMS, extract final state particles and call analyze(particles). */
-   AnalysisHandler::analyze(event, ieve, loop, status);
-   if ( loop > 0 || !event ) return;
-   // if ( loop > 0 || status != 0 || !event ) return;
+    /* Rotate to CMS, extract final state particles and call analyze(particles). */
+    AnalysisHandler::analyze(event, ieve, loop, status);
+    
+    if ( loop > 0 || status != 0 || !event ) return;
+    
+    pEvent->fWeight = event->weight();
+    
+    /* The hardest subprocess */
+    eh = event->primaryCollision()->handler();
+    const ParticleVector hardProc = event->primarySubProcess()->outgoing();
+    vector<int> outgoingHard;
+    for (std::size_t i = 0; i < hardProc.size(); ++i) {
+        outgoingHard.push_back( hardProc[i]->number() );
+        particleAdd(hardProc[i],3);
+    }
+    
+    /* Final state particles */
+    tPVector finals = event->getFinalState();
+    for (tPVector::const_iterator pit = finals.begin(); pit != finals.end(); ++pit) {
+        int absId = abs( (*pit)->id() );
+        int saveStatus = 1;
+        if ( (mode==0) && absId==ParticleID::gamma && gammaChecker(*pit) ) {
+            saveStatus = 2;
+        }
+        
+        particleAdd( *pit, saveStatus );
+    }
 
-   //tPVector parts=event->getFinalState();
-   tPVector parts;
-   event->select(std::back_inserter(parts),SelectAll());
-   eh = event->primaryCollision()->handler();
-   
-   /* Loop over all particles. */ 
-   for (tPVector::const_iterator pit = parts.begin(); pit != parts.end(); ++pit) {
-      int absId = abs( (*pit)->id() );
-      if ( absId==2101 || absId ==2203 || absId==82 ) continue; // uu, ud, p+rem
-      int pStatus = getStatusCode( *pit );
-      if ( pStatus==3 ) continue; // Beam particles and partons, not of interest
-      
-      /* Normal end state particles */
-      if (pStatus == 1) {
-         int pi0Gamma = 0;
-         if ( absId==22 && gammaChecker(*pit) ) pi0Gamma = 1;
-         pEvent->AddPrtcl((*pit)->momentum().x(),(*pit)->momentum().y(),(*pit)->momentum().z(),
-            (*pit)->momentum().t(), (*pit)->id(), (*pit)->data().charge(), pi0Gamma ? 10 : 1);
-      }
-      
-      /* Ghost particles */
-      int ghostStatus = 0;
-      
-      if ( (pStatus==2) && ( (absId<=6 && absId>0) || absId==21 ) ) {
-            ghostStatus = 11; /* Partons */
-      } else if (absId >= 100) { /* Status codes below this are not conventional hadrons */
-         if (HadrFuncs::hasBottom(absId) && !isExcitedHadronState(*pit,5) ) {
-            ghostStatus = 12; /* b Hadrons */
-         }
-         if (HadrFuncs::hasCharm(absId) && !isExcitedHadronState(*pit,4) ) {
-            ghostStatus = 13; /* c Hadrons */
-         }
-      } /* Add Leptons here if needed */
-      
-      if (ghostStatus) {
-         pEvent->AddPrtcl((*pit)->momentum().x(),(*pit)->momentum().y(),(*pit)->momentum().z(),
-            (*pit)->momentum().t(), (*pit)->id(), (*pit)->data().charge(),ghostStatus);
-      }
-   }
-   // TODO ghost particles/partons
-   
-   herwigTree->Fill();
-   pEvent->Clear();
+//     tPVector parts;
+//     event->select(std::back_inserter(parts),SelectAll());
+//     /* Loop over all particles. */ 
+//     for (tPVector::const_iterator pit = parts.begin(); pit != parts.end(); ++pit) {
+//         int absId = abs( (*pit)->id() );
+//         if ( absId==2101 || absId ==2203 || absId==82 ) continue; // uu, ud, p+rem
+//         
+//         int pStatus = getStatusCode( *pit );
+//     }
+    
+    herwigTree->Fill();
+    pEvent->Clear();
 }
 
 void StoreParticles::dofinish() 
 {
-  AnalysisHandler::dofinish();
-  
-  herwigTree->GetCurrentFile();
-  herwigTree->AutoSave("Overwrite");
-  herwigFile->Close();
-  cout << "StoreParticles: root tree has been written to a file" << endl;  
+    AnalysisHandler::dofinish();
+    
+    herwigTree->GetCurrentFile();
+    herwigTree->AutoSave("Overwrite");
+    herwigFile->Close();
+    cout << "StoreParticles: a root tree has been written to a file" << endl;  
 }
 
 void StoreParticles::doinitrun() 
 {
     AnalysisHandler::doinitrun();
     string fileName = "particles_herwig";
+    mode = 0;
+    cout << "halp" << endl;
     
+    /* In a general multithread-case, generate a thread-unique root file name */
     string runInfo = generator()->runName();
     size_t pos = runInfo.find("++")+2;
     if (runInfo.size() > 0) {
@@ -132,12 +151,22 @@ void StoreParticles::doinitrun()
         
         runInfo = runInfo.substr(pos+1);
         pos = runInfo.find("_");
-        int mode = std::stoi(runInfo.substr(0,pos));
+        mode = std::stoi(runInfo.substr(0,pos));
         
         fileName += "_";
         if (mode == 0) {
             fileName += "generic_";
+        } else if (mode == 1) {
+            fileName += "dijet_";
+        } else if (mode == 2) {
+            fileName += "gammajet_";
+        } else if (mode == 3) {
+            fileName += "Zjet_";
+        } else {
+            cout << "Bad input file" << endl;
+            fileName += "dijet_";
         }
+        
         string fileNameFinal = fileName;
         fileName += std::to_string( generator()->N() );
         if (multiplier > 1) {
@@ -154,18 +183,18 @@ void StoreParticles::doinitrun()
     }
     fileName += ".root";
     
+    /* Setup a root tree in the selected file */
     herwigFile = new TFile (fileName.c_str(),"RECREATE");
     herwigFile->SetCompressionLevel(1); // by default file is compressed 
 
     if (!herwigFile) {
-        cout << "StoreParticles: root file has not been created..." << endl;
+        cout << "StoreParticles: root file has not been created." << endl;
         return;
     }
     
-    // create ROOT Tree
     herwigTree = new TTree ("HerwigTree","Tree filled with herwig data.");
     if (!herwigTree) {
-        cout << "StoreParticles: root tree has not been created..." << endl;
+        cout << "StoreParticles: root tree has not been created." << endl;
         return;
     }
     herwigTree->SetAutoSave(100000000); /* autosave when 0.1 Gbyte written */
@@ -176,14 +205,9 @@ void StoreParticles::doinitrun()
     TBranch *branch = herwigTree->Branch("event", &pEvent, 32000,4);
     branch->SetAutoDelete(kFALSE);
     herwigTree->BranchRef();
-    
 }
 
-/* *** Attention *** The following static variable is needed for the type
- * description system in ThePEG. Please check that the template arguments
- * are correct (the class and its base class), and that the constructor
- * arguments are correct (the class name and the name of the dynamically
- * loadable library where the class implementation can be found). */
+/* *** Attention *** This class-description must be correct. */
 DescribeNoPIOClass<StoreParticles,AnalysisHandler>
   describejetanalysisStoreParticles("jetanalysis::StoreParticles", "../lib/libStoreParticles.so");
 
--- lhc_simulation/herwig/StoreParticles.h
+++ lhc_simulation/herwig/StoreParticles.h
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////
 // A class for Herwig to store simulation data into a .root file //
 // Based on a template: contains some peculiarities              //
-// Hannu Siikonen 7.3.2015                                       //
+// Hannu Siikonen 5.6.2015                                       //
 ///////////////////////////////////////////////////////////////////
 
 #ifndef STOREPARTICLES_H
@@ -11,193 +11,99 @@
 #include "ThePEG/EventRecord/Event.h"
 #include "ThePEG/EventRecord/Particle.h"
 #include "ThePEG/EventRecord/StandardSelectors.h"
+#include "ThePEG/Interface/ClassDocumentation.h"
+#include "ThePEG/EventRecord/Particle.h"
+#include "ThePEG/Repository/UseRandom.h"
+#include "ThePEG/Repository/EventGenerator.h"
+#include "ThePEG/Utilities/DescribeClass.h"
+#include "ThePEG/PDT/StandardMatchers.h"
 
-#include <iostream>
-using std::cout;
-using std::endl;
+#include "../generic/help_functions.h"
 
-// ROOT:
 #include "TTree.h"
 #include "TFile.h"
 #include "../events/PrtclEvent.h"
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <cstdlib>
+#include <cassert>
+using std::cout;
+using std::endl;
 
 namespace jetanalysis {
 
 using namespace ThePEG;
 
-/**
- * Here is the documentation of the StoreParticles class.
- *
- * @see \ref StoreParticlesInterfaces "The interfaces"
- * defined for StoreParticles.
- */
 class StoreParticles: public AnalysisHandler {
 
 public:
 
-   /** @name Standard constructors and destructors. */
-   //@{
-   StoreParticles() {}
-   
-   ~StoreParticles() {}
-   //@}
-   
-   /** @name Virtual functions required by the AnalysisHandler class. */
-   //@{
-   /**
-    * Analyze a given Event. Note that a fully generated event
-    * may be presented several times, if it has been manipulated in
-    * between. The default version of this function will call transform
-    * to make a lorentz transformation of the whole event, then extract
-    * all final state particles and call analyze(tPVector) of this
-    * analysis object and those of all associated analysis objects. The
-    * default version will not, however, do anything on events which
-    * have not been fully generated, or have been manipulated in any
-    * way.
-    * @param event pointer to the Event to be analyzed.
-    * @param ieve the event number.
-    * @param loop the number of times this event has been presented.
-    * If negative the event is now fully generated.
-    * @param state a number different from zero if the event has been
-    * manipulated in some way since it was last presented.
-    */
-   virtual void analyze(tEventPtr event, long ieve, int loop, int state);
-
-   /**
-    * Return a LorentzTransform which would put the event in the
-    * desired Lorentz frame.
-    * @param event a pointer to the Event to be considered.
-    * @return the LorentzRotation used in the transformation.
-    */
-   LorentzRotation transform(tcEventPtr) const { return LorentzRotation(); }
-
-   /**
-    * Analyze the given vector of particles. The default version calls
-    * analyze(tPPtr) for each of the particles.
-    * @param parts the vector of pointers to particles to be analyzed
-    * @param weight the weight of the current event.
-    */
-   void analyze(const tPVector & parts, double) { AnalysisHandler::analyze(parts); }
-
-   /**
-    * Analyze the given particle.
-    * @param particle pointer to the particle to be analyzed.
-    * @param weight the weight of the current event.
-    */
-   void analyze(tPPtr, double) {}
-   //@}
+    StoreParticles() {}
+    
+    ~StoreParticles() {}
+    
+    /** Analyze a given Event.
+        * @param event pointer to the Event to be analyzed.
+        * @param ieve the event number.
+        * @param loop the number of times this event has been presented.
+        * @param state nonzero if the event has been manipulated. */
+    virtual void analyze(tEventPtr event, long ieve, int loop, int state);
 
 
-   /**
-    * The standard Init function used to initialize the interfaces.
-    * Called exactly once for each class by the class description system
-    * before the main function starts or
-    * when this class is dynamically loaded.
-    */
-   static void Init()
-   {
-      static ClassDocumentation<StoreParticles> documentation
-         ("There is no documentation for the StoreParticles class");
-   }
+    /** Standard Init function, called exactly once. */
+    static void Init() { static ClassDocumentation<StoreParticles> documentation("No documentation"); }
 
 protected:
 
-   /**
-    * ThePEG uses surprisingly non-trivial structures for fetching the particle
-    * status and normally this is not calculated or stored at all during the 
-    * run. Fortunately, when the data is turned into HepMC format, the status 
-    * codes are calculated. See ThePEG sources Vectors/HepMCConverter.h and
-    * HepMCConverter.tcc. This is a mock-up of the things done in the member
-    * function createParticle, with all unnecessary things taken out. Even
-    * more untrivial: the CMSSW code uses a bit different interface - this 
-    * affects the Jet flavour definition. See 
-    * cmssw/GeneratorInterfacce/ThePEGInterface/src/HepMCConverter.cc
-    */
-   int getStatusCode( tPPtr ) const;
-   
-   /** @name Clone Methods. */
-   //@{
-   /**
-    * Make a simple clone of this object.
-    * @return a pointer to the new object.
-    */
-   IBPtr clone() const { return new_ptr(*this); }
+    /** @name Setup and finalization of the run */
+    //@{
+    
+    /** Initialize this object. Called in the run phase just before a run begins. */
+    virtual void doinitrun();
 
-   /** Make a clone of this object, possibly modifying the cloned object
-    * to make it sane.
-    * @return a pointer to the new object.
-    */
-   IBPtr fullclone() const { return new_ptr(*this); }
-   //@}
+    /** Finalize this object. Called in the run phase just after a run has ended. */
+    virtual void dofinish();
+    
+    //@}
+    
+    /** @name Help methods for the analysis */
+    //@{
+    
+    /** ThePEG does not provide useful status codes and the status has to be studied manually.
+      * This method is a mock-up of the CMSSW-way to calculate the status code. However, these
+      * codes are not at the moment in use and the method is here only for reference */
+    int getStatusCode(const tPPtr&) const;
 
-   /** @name Standard Interfaced functions. */
-   //@{
-   /**
-   * Check sanity of the object during the setup phase. Does nothing at the moment.
-   */
-   void doupdate()
-   {
-      AnalysisHandler::doupdate();
-      bool redo = touched(); if ( !redo ) return;
-   }
-   
-  /**
-   * Initialize this object after the setup phase before saving an
-   * EventGenerator to disk.
-   * @throws InitException if object could not be initialized properly.
-   */
-  void doinit() { AnalysisHandler::doinit(); }
+    int gammaChecker(const tPPtr&);
+    
+    int isExcitedHadronState(const tPPtr&, int);
+    
+    void particleAdd(const tPPtr&, int);
+    
+    //@}
+    
+    /** @name Variables for the analysis */
+    //@{
+    
+    /* The hardest event */
+    tcEventBasePtr eh;
+    
+    int mode;
 
-  /**
-   * Initialize this object. Called in the run phase just before
-   * a run begins.
-   */
-  virtual void doinitrun();
-
-  /**
-   * Finalize this object. Called in the run phase just after a
-   * run has ended. Used eg. to write out statistics.
-   */
-  virtual void dofinish();
-
-  /**
-   * Rebind pointer to other Interfaced objects. Called in the setup phase
-   * after all objects used in an EventGenerator has been cloned so that
-   * the pointers will refer to the cloned objects afterwards.
-   * @param trans a TranslationMap relating the original objects to
-   * their respective clones.
-   * @throws RebindException if no cloned object was found for a given
-   * pointer.
-   */
-  void rebind(const TranslationMap & trans) { AnalysisHandler::rebind(trans); }
-
-  /**
-   * Return a vector of all pointers to Interfaced objects used in this
-   * object.
-   * @return a vector of pointers.
-   */
-  virtual IVector getReferences()
-  {
-  IVector ret = AnalysisHandler::getReferences();
-  return ret;
-  }
-  //@}
-
-  
+    /* Data handle for ROOT */
+    PrtclEvent *pEvent;
+    
+    /* ROOT, saving data */
+    TTree * herwigTree;
+    TFile * herwigFile;
+    
+    //@}
 private:
-
-   /**
-    * The assignment operator is private and must never be called.
-    * In fact, it should not even be implemented.
-    */
-   StoreParticles & operator=(const StoreParticles &);
-
-   tcEventBasePtr eh;
-   
-   TTree * herwigTree;
-   TFile * herwigFile;
-
-   PrtclEvent *pEvent;
+    /* The assignment operator is private and must never be called nor implemented. */
+    StoreParticles & operator=(const StoreParticles &);
 };
 
 }
