efe9962ed6152806b50c49329c67a210106a6f97
Hannu Siikonen <hannu.siikonen@aalto.fi>
2016-02-02 17:51:00 +0200

    doo

--- lhc_simulation/bash/p8_series.sh
+++ lhc_simulation/bash/p8_series.sh
@@ -4,11 +4,10 @@ NUM_EVT=$1
 NUM_LAYERS=$2
 JOB_TYPE=$3
 NUM_PROC=8
-DIVINE_IDX=0
 
 EVT_PER_LAYER=$(($NUM_EVT/$NUM_LAYERS))
 
-for (( i=1; i<=$NUM_LAYERS; i++ ))
+for (( i=0; i<$NUM_LAYERS; i++ ))
 do
-    bsub -n $NUM_PROC -q 1nw -R "pool>20000" pythia8_lxplus.sh $EVT_PER_LAYER $JOB_TYPE $NUM_PROC $i
+    bsub -n $NUM_PROC -q 1nw -R "pool>20000" pythia8_lxplus.sh $EVT_PER_LAYER $JOB_TYPE $NUM_PROC $(($i*$NUM_PROC))
 done
