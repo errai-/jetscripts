83ef0dbd55f07a5830871e1387e68b13ed1d7ca0
Hannu Siikonen <hannu.siikonen@cern.ch>
2016-02-02 09:44:31 +0100

    LXPLUS settings

--- lhc_simulation/config.mk
+++ lhc_simulation/config.mk
@@ -14,7 +14,8 @@ CFLAGS :=
 CFLAGS := $(CFLAGS) -g -Wall -Wextra -Werror -Wshadow -Wno-error=unused-parameter -Wno-error=unused-function -Wno-error=unknown-pragmas -march=native -O3 -pedantic -m64
 CXXFLAGS := $(CFLAGS) -std=c++11
 
-PYTHIA6:=-lEG -lEGPythia6 -L/home/hannu/Cern/pythia6 -lPythia6
+PYTHIA6:=-lEG -lEGPythia6 -L/afs/cern.ch/user/h/hsiikone/Cern/pythia6 -lPythia6
+//PYTHIA6:=-lEG -lEGPythia6 -L/home/hannu/Cern/pythia6 -lPythia6
 PYTHIA8:=$(shell pythia8-config --cflags --libs)
 FASTJET:=$(shell fastjet-config --cxxflags --libs)
 ROOT:=$(shell root-config --cflags --glibs) -lGenVector
