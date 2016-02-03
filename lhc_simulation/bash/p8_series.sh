632bf38af8d4a3fe96841097f88ae447db5dc0ae
Hannu Siikonen <hannu.siikonen@cern.ch>
2016-02-03 10:19:23 +0100

    more locality

--- lhc_simulation/bash/p8_series.sh
+++ lhc_simulation/bash/p8_series.sh
@@ -9,5 +9,5 @@ EVT_PER_LAYER=$(($NUM_EVT/$NUM_LAYERS))
 
 for (( i=0; i<$NUM_LAYERS; i++ ))
 do
-    bsub -n $NUM_PROC -q 1nh -R "pool>20000" pythia8_lxplus.sh $EVT_PER_LAYER $JOB_TYPE $NUM_PROC $(($i*$NUM_PROC))
+    bsub -n $NUM_PROC -q 2nw -R "pool>20000" pythia8_lxplus.sh $EVT_PER_LAYER $JOB_TYPE $NUM_PROC $(($i*$NUM_PROC))
 done
